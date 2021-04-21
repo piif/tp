c08a7e64f663c6b5bb83cf12238a0132ab0496e9
christian lefebvre (fr20311) <christian.lefebvre@worldline.com>
2021-04-21 18:00:50 +0200

    port ghost block in tetra version

--- Board.java
+++ Board.java
@@ -32,27 +32,36 @@ class Board extends Canvas {
         return TILE_SIZE * (lines + 2);
     }
     
-    private void drawTile(Graphics g, int col, int line, Color color) {
-        g.setColor(color);
-        g.fill3DRect(col*TILE_SIZE, line*TILE_SIZE, TILE_SIZE, TILE_SIZE, true);
+    private void drawTile(Graphics g, int col, int line, int color) {
+        if (color == -1) {
+            g.setColor(Color.lightGray);
+            g.drawRect(col*TILE_SIZE, line*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        } else {
+            g.setColor(COLORS[color % COLORS.length]);
+            g.fill3DRect(col*TILE_SIZE, line*TILE_SIZE, TILE_SIZE, TILE_SIZE, true);
+        }
     }
 
-    public void drawBlock(Block block, int pos, int col, int line) {
+    public int getTile(int col, int line) {
+        return tiles[line][col];
+    }
+
+    private void drawBlock(Block block, int pos, int col, int line, int color) {
         // System.out.println("drawBlock "+pos+", "+col+", "+line);
         Block.Position position = block.positions[pos];
         for (int t=0; t<4; t++) {
             Block.Tile tile = position.tiles[t];
-            tiles[line+tile.dy][col+tile.dx] = block.color;
+            tiles[line+tile.dy][col+tile.dx] = color;
         }
     }
-
+    public void drawBlock(Block block, int pos, int col, int line) {
+        drawBlock(block, pos, col, line, block.color);
+    }
+    public void drawGhost(Block block, int pos, int col, int line) {
+        drawBlock(block, pos, col, line, -1);
+    }
     public void removeBlock(Block block, int pos, int col, int line) {
-        // System.out.println("removeBlock "+pos+", "+col+", "+line);
-        Block.Position position = block.positions[pos];
-        for (int t=0; t<4; t++) {
-            Block.Tile tile = position.tiles[t];
-            tiles[line+tile.dy][col+tile.dx] = 0;
-        }
+        drawBlock(block, pos, col, line, 0);
     }
 
     public boolean checkBlock(Block block, int pos, int col, int line) {
@@ -66,7 +75,7 @@ class Board extends Canvas {
             if (c < 0 || c >= columns) {
                 return false;
             }
-            if (tiles[line+tile.dy][col+tile.dx] != 0) {
+            if (tiles[l][c] != 0) {
                 return false;
             }
         }
@@ -99,7 +108,7 @@ class Board extends Canvas {
             for (int col = 0; col < columns; col++) {
                 // remove "if" to display dark blocks, but implies flickering
                 if (tiles[line][col] != 0) {
-                    drawTile(g, col+1, line+1, COLORS[(tiles[line][col]) % COLORS.length]);
+                    drawTile(g, col+1, line+1, tiles[line][col]);
                 }
             }
         }
